df8d15bc9396b6da674daf2503df1e5cd745cd04
Ilia Shurov <shurov@hotmail.com>
2024-08-01 23:38:42 +0200

    Add struct

--- Main.go
+++ Main.go
@@ -5,11 +5,45 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                   string
+	age                    uint16
+	money                  int16
+	avg_grandes, happiness float64
+}
+
+func (u User) gerAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
+		"equal: %d", u.name, u.age, u.money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	bob.setNewName("Alex")
+	fmt.Fprintf(w, bob.gerAllInfo())
+}
+
+func about_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About page")
+}
+
+func blog_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Blog page")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/about", about_page)
+	http.HandleFunc("/blog", blog_page)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	http.HandleFunc("/", home_page)
-	http.ListenAndServe(":8080", nil)
+	//bob := User{name: "Bob", age: 25, money: -50, avg_grandes: 4.2, happiness: 0.8}
+
+	handleRequest()
 }
